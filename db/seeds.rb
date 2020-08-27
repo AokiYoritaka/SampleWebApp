705fc9fdcf40993f580ba98fe777f1110d2ebfc2
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-28 00:22:28 +0900

    gem therubyracer削除

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,1 @@
-Pofile.create(name: 'yori', age: '5', liveplace: 'Tokyo', introduce: 'aaaaa')
-Pofile.create(name: 'yora', age: '3', liveplace: 'Tokyo', introduce: 'aaaaa')
-Pofile.create(name: 'yorf', age: '4', liveplace: 'Tokyo', introduce: 'aaaaa')
-Pofile.create(name: 'yord', age: '6', liveplace: 'Tokyo', introduce: 'aaaaa')
+Profile.create(user_id: 1, name: 'yori', age: '5', liveplace: 'Tokyo', introduce: 'aaaaa')
