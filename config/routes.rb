9e140de448a864cdbcba5f576165ac33a32911ba
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-28 01:13:18 +0900

    routes.rbにomniauth_callbacks追加

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,12 @@ Rails.application.routes.draw do
   root "top#index"
   #devise 
   devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks",
     registrations: 'users/registrations',
     confirmations: 'users/confirmations',
   }
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
-
   resources :profiles, only: [:show, :edit, :update]
-
 end
