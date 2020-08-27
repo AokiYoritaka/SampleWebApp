a58af6ecedcbccebc7c3999a0f17d818e14c6a72
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-28 01:11:02 +0900

    user.rbにself.from_omniauthアクション追加

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,6 +6,15 @@ class User < ApplicationRecord
          :confirmable, :omniauthable, :omniauthable_providers: [:twitter]
   has_one :profile, dependent: :destroy
 
+  def self.from_omniauth(auth)
+    find_or_create_by(provider: auth["provider"], uid: auth["uid"]) do |user|
+      user.provider = auth["provider"]
+      user.uid = auth["uid"]
+      user.username = auth["info"]["nickname"]
+      user.skip_confirmation!
+    end
+  end
+
   def self.guest
     find_or_create/by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
