b4749faa44bea410f2b852ff9d4e7574eae686fe
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-27 17:42:08 +0900

    profiles_controllerにshow, edit, action追加

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -1,5 +1,16 @@
 class ProfilesController < ApplicationController
   before_action :validate_user, only: [:edit, :update]
 
-  
+  def show
+    @profile = Profile.find(params[:id])
+  end
+
+  def edit
+    @profile = Profile.find(params[:id])
+  end
+
+  def update
+    @profile = Profile.find(params[:id])
+  end
+
 end
