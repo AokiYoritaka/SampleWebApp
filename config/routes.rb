603f6d0d0c603da6cd551ee9903312a7465278cb
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-27 12:11:10 +0900

    confirmation_controller修正

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,14 @@ Rails.application.routes.draw do
   root to: 'top#index'
   resources :health_check, only: [:index]
 
-
+  #devise
+  devise_for :users, controllers: {
+    omniauth_callbacks: "omniauth_callbacks",
+    registrations: 'users/registrations',
+    confirmations: 'users/confirmations',
+  }
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#new_guest'
+  end
 
 end
