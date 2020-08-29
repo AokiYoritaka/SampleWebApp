918fb02418fc6b32af7e2bd146ed70fa088a94a0
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-29 16:05:21 +0900

    reviewのmigration追加

--- db/migrate/20200829065409_create_reviews.rb
+++ db/migrate/20200829065409_create_reviews.rb
@@ -1,6 +1,9 @@
 class CreateReviews < ActiveRecord::Migration[5.2]
   def change
     create_table :reviews do |t|
+      t.string :name
+      t.string :title
+      t.text :body
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_041430) do
+ActiveRecord::Schema.define(version: 2020_08_29_065409) do
 
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id"
@@ -21,6 +21,14 @@ ActiveRecord::Schema.define(version: 2020_08_29_041430) do
     t.string "liveplace"
     t.text "introduce"
     t.index ["user_id"], name: "index_profiles_on_user_id"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "name"
+    t.string "title"
+    t.text "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "schools", force: :cascade do |t|
