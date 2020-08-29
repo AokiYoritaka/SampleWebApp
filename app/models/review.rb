01ee66b4a64f98dffd0a8406d634f3e9ada15844
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-29 22:32:13 +0900

    review index.html修正

--- app/models/review.rb
+++ app/models/review.rb
@@ -4,6 +4,5 @@ class Review < ApplicationRecord
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true
   has_many :review_images, dependent: :destroy
-  accepts_attachments_for :review_images, attachment: :image
   has_many :comments, dependent: :destroy
 end
