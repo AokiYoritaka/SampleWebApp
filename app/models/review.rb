4d9f26e75c283988a1cf2b7e89711ca942516078
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-29 17:09:09 +0900

    reviwe.rb修正

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,9 @@
 class Review < ApplicationRecord
   belongs_to :user
+  belongs_to :school
   validates :title, presence: true, length: { maximum: 50 }
   validates :body, presence: true
-  belongs_to :school
+  has_many :review_images, dependent: :destroy
+  accepts_attachments_for :review_images, attachment: :image
+  
 end
