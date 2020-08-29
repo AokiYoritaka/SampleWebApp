a734680636dcb1841803fb2ff9a9b4a319c9f2f4
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-29 17:17:17 +0900

    migrationファイルにreview_images追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_08_29_065409) do
+ActiveRecord::Schema.define(version: 2020_08_29_080110) do
 
   create_table "profiles", force: :cascade do |t|
     t.integer "user_id"
@@ -23,12 +23,22 @@ ActiveRecord::Schema.define(version: 2020_08_29_065409) do
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
+  create_table "review_images", force: :cascade do |t|
+    t.string "review_id"
+    t.string "image_id"
+    t.integer "review_images"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "reviews", force: :cascade do |t|
     t.string "name"
     t.string "title"
     t.text "body"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
   create_table "schools", force: :cascade do |t|
