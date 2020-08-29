4e57172d1f375f07b55ccbde2776c1f41953487c
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-29 23:23:40 +0900

    like コントローラ修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,9 +2,16 @@ class LikesController < ApplicationController
   before_action :sign_in_required
     def create
       @review = Review.find(params[:review_id])
+      unless @review.like_rev?(current_user)
+        @review.like_rev(current_user)
+      end
     end
 
     def destroy
       @review = Like.find(params[:id]).review
+      if @review.like_rev?(current_user)
+        @review.un_like_rev(current_user)
+        @review.reload
+      end
     end
 end
