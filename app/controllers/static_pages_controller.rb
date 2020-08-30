50d3dfe5583361cffd9f792da4dd46d45b5f227b
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-30 11:55:43 +0900

    StaticPage startアクション

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,4 +1,11 @@
 class StaticPagesController < ApplicationController
   def start
+    if user_signed_in?
+      redirect_to root_path
+    else
+      @reviews = Review.order(created_at "DESC").page(params[:page]).per(5)
+    end
   end
+
+  
 end
