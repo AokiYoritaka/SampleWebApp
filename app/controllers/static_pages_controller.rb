7146389d1e1ff836b2a2aec6401b4d534daccfae
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-30 11:57:00 +0900

    StaticPage homeアクション

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,5 +7,17 @@ class StaticPagesController < ApplicationController
     end
   end
 
-  
+  def home
+    if user_signed_in?
+      @reviews = Review.order(created_at: "DESC").page(params[:page]).per(3)
+      if params[:tag_name]
+        @reviews = Review.tagged_with("#{params[:tag_name]}").order(created_at: "DESC").page(params[:page]).per(3)
+        @tagname = params[:tag_name]
+      end
+    else
+      redirect_to start_path
+    end
+  end
+
+
 end
