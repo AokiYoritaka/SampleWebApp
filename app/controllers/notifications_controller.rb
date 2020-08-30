188fbcea5ff93b31720255ca287eb4150f03b305
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-30 12:28:46 +0900

    notification indexアクション追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,3 +1,9 @@
 class NotificationsController < ApplicationController
-  
+  def index
+    #current_userの投稿に紐づいた通知一覧
+    @notifications = current_user.passive_notifications.page(params[:page]).per(20)
+    @notifications.where(checked: false).each do |notification|
+      notification.update_attributes(checked: true)
+    end
+  end
 end
