21924be3f076893d3ceb92feb1592138eefd9483
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-30 22:57:01 +0900

    category show, newアクション

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,5 +3,15 @@ class CategoriesController < ApplicationController
     @categories = Category.all
   end
 
+  def show
+    @category = Category.find(params[:id])
+    @reviews = @category.reviews.order(created_at: "DESC").page(params[:page]).per(3)
+  end
+
+  def new
+    @categories = Category.all
+    @category = Category.new
+  end
   
+
 end
