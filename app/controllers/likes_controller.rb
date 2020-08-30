abfe88016c91eda6b79d4bcb00a48c93789b39b9
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-30 12:32:46 +0900

    like createアクション修正

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,9 +1,12 @@
 class LikesController < ApplicationController
   before_action :sign_in_required
+
     def create
       @review = Review.find(params[:review_id])
       unless @review.like_rev?(current_user)
         @review.like_rev(current_user)
+        @review.create_notification_like!(current_user)
+        @review.reload
       end
     end
 
