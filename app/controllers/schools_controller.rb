d9a90727ed90e36ee2df0832d368d60b9b313b84
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-30 17:58:03 +0900

    school school_search_paramsアクション追加

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -1,18 +1,32 @@
 class SchoolsController < ApplicationController
+  before_action :sign_in_required, only: [:new]
 
   def index
-    @schools = School.order(created_at: "DESC").page(params[:page]).per(10)
+    @search_params = school_search_params
+    @schools = school.search(@search_params).order(created_at: "DESC").page(params[:page]).per(6)
+    all_genre = School.pluck(:genre) + School.pluck(:subgenre)
+    @genres = all_genre.uniq.reject(&:blank?)
+    all_prefecture = School.pluck(:prefecture)
+    @prefectures = all_prefecture.uniq.reject(&:blank?)
   end
 
   def show
     @schools = School.find(params[:id])
+    @reviews = @school.reviews.order(created_at: "DESC").page(params[:page]).per(3)
   end
 
   def new
     @school = School.new
+    @user = User.last
   end
 
   def create
     
+  end
+
+  private
+
+  def school_search_params
+    params.fetch(:search. {}).permit(:name, :prefecture, :genre)
   end
 end
--- app/models/school.rb
+++ app/models/school.rb
@@ -2,5 +2,10 @@ class School < ApplicationRecord
   has_many :review, dependent: :destroy
   scope :sorted, -> { order(created_at: :desc) }
   validates :name, presence: true
-  
+
+  scope :search, -> (search_params) do
+    return if search_params.blank?
+    name_like(search_params[:name]).prefecture_is(search_params[:prefecture]).genre_is(search_params[:genre])
+  end
+
 end
