39e8bbcc438ab197739c3e7e9b7b2921ef01dea1
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-08-31 12:52:52 +0900

    registration_controller修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,1 +1,3 @@
 Profile.create(user_id: 1, name: 'yori', age: '5', liveplace: 'Tokyo', introduce: 'aaaaa')
+Profile.create(user_id: 2, name: 'ori', age: 'f', liveplace: 'Tokyo', introduce: 'aaaaa')
+
