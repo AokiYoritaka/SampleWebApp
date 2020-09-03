83be304f45cc15ec053388c7daca1b46c7dc69e5
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-09-03 16:22:53 +0900

    error.js.erb修正

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,6 +1,6 @@
 class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :review
-  validates :body, presence: true, length: { maximum: 200 }
   has_many :notifications, dependent: :destroy
+  validates :body, presence: true, length: { maximum: 200 }
 end
