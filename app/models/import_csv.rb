b145f445412939f56c435c0c7ba7706a872b9a7b
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-09-03 21:38:12 +0900

    import_csv.rb修正

--- app/models/import_csv.rb
+++ app/models/import_csv.rb
@@ -1,6 +1,6 @@
 class ImportCsv < ApplicationRecord
   # CSVデータのパスを引数として受け取り、インポート処理を実行
-  def self.users_data
+  def self.schools_data
     # インポートするファイルのパスを取得
     path = File.join Rails.root, "db/csv/csv_data.csv"
     # インポートするデータを格納するための配列
@@ -9,13 +9,25 @@ class ImportCsv < ApplicationRecord
     CSV.foreach(path, headers: true) do |row|
       list << {
           name: row["name"],
-          email: row["email"],
+          address: row["address"],
+          tell: row["tell"],
+          genre: row["genre"],
+          detail: row["detail"],
+          image_url_a: row["image_url_a"],
+          image_url_b: row["image_url_b"],
+          created_at: row["created_at"],
+          updated_at: row["updated_at"],
+          latitude: row["latitude"],
+          longitude: row["longitude"],
+          opentime: row["opentime"],
+          subgenre: row["subgenre"],
+          res_id: row["res_id"],
       }
     end
     puts "インポート処理を開始"
     # インポートができなかった場合の例外処理
     begin
-      User.create!(list)
+      school.create!(list)
       puts "インポート完了!!"
     rescue ActiveModel::UnknownAttributeError => invalid
       puts "インポートに失敗：UnknownAttributeError"
