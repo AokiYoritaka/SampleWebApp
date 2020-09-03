d3b06072e011e47c58c925bc1253d498fe5a280b
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-09-03 18:24:15 +0900

    detailsのmap情報修正

--- app/models/school.rb
+++ app/models/school.rb
@@ -1,7 +1,7 @@
 class School < ApplicationRecord
   has_many :reviews, dependent: :destroy
   geocoded_by :address
-  after_validation :geocode
+  after_validation :geocode, if: :address_changed?
   validates :name, presence: true
   validates :address, presence: :true
   validates :res_id, uniqueness: true
