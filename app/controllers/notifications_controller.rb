1f6dc8ca878f5e75c432fbda850ff0e0501381ad
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-09-07 19:35:04 +0900

    slider.html追加

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
   def index
-    #current_userの投稿に紐づいた通知一覧
+    #current_userの投稿に紐づいた通知リスト
     @notifications = current_user.passive_notifications.page(params[:page]).per(20)
     @notifications.where(checked: false).each do |notification|
       notification.update_attributes(checked: true)
