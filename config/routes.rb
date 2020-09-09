9c872843c9d6284ea5e79feee34fe524ef34825a
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-09-10 00:52:43 +0900

    sessions_contorller追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,8 @@
 Rails.application.routes.draw do
-  root 'static_pages#home'
-  get "start", to: "static_pages#start"
   get 'likes/create'
   get 'likes/destroy'
   get 'reviews/index'
-
-  #devise 
+  get "start", to: "static_pages#start"
   devise_for :users, controllers: {
     omniauth_callbacks: "omniauth_callbacks",
     registrations: 'users/registrations',
@@ -14,16 +11,16 @@ Rails.application.routes.draw do
   devise_scope :user do
     post 'users/guest_sign_in', to: 'users/sessions#new_guest'
   end
+  get "alltags", to: "static_pages#alltags"
+  get "search", to: "reviews#search"
+  root 'static_pages#home'
   resources :schools do
     resources :reviews do
       resources :comments
     end
   end
-
   resources :likes, only: [:destroy, :create]  
-  resources :profiles, only: [:show, :edit, :update]
   resources :notifications
   resources :categories, only: [:index, :show, :new, :create, :destroy]
-  get "alltags", to: "static_pages#alltags"
-  get "search", to: "reviews#search"
+  resources :profiles, only: [:show, :edit, :update]
 end
