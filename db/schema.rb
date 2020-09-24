a2c9502911e72f830c006b050bda70251a0d11e1
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-09-24 23:29:35 +0900

    profile.rb mount_uploader追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_09_143516) do
+ActiveRecord::Schema.define(version: 2020_09_24_061813) do
 
   create_table "active_storage_attachments", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name", null: false
@@ -81,6 +81,7 @@ ActiveRecord::Schema.define(version: 2020_09_09_143516) do
     t.datetime "updated_at", null: false
     t.string "age"
     t.text "introduce"
+    t.string "avatar"
     t.index ["user_id"], name: "index_profiles_on_user_id"
   end
 
