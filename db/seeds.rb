c536bcc3f96d94080706623201182aab65ee0974
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-10-04 17:26:23 +0900

    school index カードcss編集

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,27 +1,7 @@
-User.create!(
-  email: "gaze25@okinawa.li",
-  password: "lalala1",
-  provider: "aoki",
-  uid: "20",
-  username: "aokikunn",
-  avatar: File.open("./app/assets/images/default.jpg")
-)
-
 Review.create!(
   name: "aokikun",
   title: "iine",
   body: "iiyo",
   user_id: "20",
   school_id: "1"
-)
-
-Profile.create!(
-  user_id: "20",
-  name: "aokikun",
-  age: "20~24歳",
-  introduce: "saikou"
-)
-
-Review.users.create!(
-  avatar: File.open("./app/assets/images/default.jpg")
 )
