0f342bfe44f3803942dbcccc8838b7033346bba2
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-10-05 22:38:46 +0900

    css修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-Review.create!(
-  name: "aokikun",
-  title: "iine",
-  body: "iiyo",
-  user_id: "20",
-  school_id: "1"
+Category.create(
+  name: "日本人が少ない"
+)
+Category.create(
+  name: "スパルタ"
+)
+Category.create(
+  name: "外国人が多い"
+)
+Category.create(
+  name: "英語に集中できる"
+)
+Category.create(
+  name: "ビーチが近い"
 )
