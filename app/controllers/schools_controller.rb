857e77f1ffc69eeaa66773796a9452d6ef6a3641
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-10-10 11:26:48 +0900

    notification修正

--- app/controllers/schools_controller.rb
+++ app/controllers/schools_controller.rb
@@ -5,7 +5,7 @@ class SchoolsController < ApplicationController
   def index
     @search_params = school_search_params
     @schools = School.search(@search_params).order(created_at: "DESC").page(params[:page]).per(9)
-    all_genre = School.pluck(:genre) + School.pluck(:subgenre)
+    all_genre = School.pluck(:subgenre)
     @genres = all_genre.uniq.reject(&:blank?)
     all_nation = School.pluck(:nation)
     @nations = all_nation.uniq.reject(&:blank?)
@@ -17,20 +17,20 @@ class SchoolsController < ApplicationController
   end
   
   def new
-#    @school = School.find_by(name: params[:name])
-#    @user = User.last
-#    respond_to do |format|
-#      format.html
-#      format.json
-#    end
-  end
-
-  def search
-    @school = School.find_by(name: params[:name])
+    @school = School.new
     @user = User.last
     respond_to do |format|
       format.html
-      format.json{render json:{school:[@school]}}
+      format.json
+    end
+  end
+
+  def search
+    @school = School.where(name: params[:name])
+    @user = User.last
+    respond_to do |format|
+      format.html
+      format.json{render json:{school:@school}}
     end
   end
 
