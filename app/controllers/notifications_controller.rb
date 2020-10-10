e8084570b67a5eccced90d025e33de5a3abd4a8f
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-10-10 17:40:11 +0900

    notification いいね修正

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,8 +1,8 @@
-class NotificationsController < ApplicationController
-  def index
-    @notifications = current_user.passive_notifications.order(created_at: "DESC").page(params[:page]).per(10)
-    @notifications.where(checked: false).each do |notification|
-      notification.update_attributes(checked: true)
+  class NotificationsController < ApplicationController
+    def index
+      @notifications = current_user.passive_notifications.order(created_at: "DESC").page(params[:page]).per(10)
+      @notifications.where(checked: false).each do |notification|
+        notification.update_attributes(checked: true)
+      end
     end
   end
-end
