edc318965ce0538d56c7d2c238e9d37ded0f8d0a
yori-goreng <61875210+Yori-goreng@users.noreply.github.com>
2020-10-28 15:07:02 +0900

    meta-tags設定

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,4 +1,25 @@
 module ApplicationHelper
+  def default_meta_tags
+    {
+      title:       "School Review",
+      description: "フィリピン／オーストラリアの語学学校レビューアプリ",
+      keywords:    "フィリピン,オーストラリア,語学学校, 留学エージェント, Ruby, Rails",
+      icon: image_url("/images/favicon.ico"), # favicon
+      noindex: ! Rails.env.production?, # production環境以外はnoindex  
+      charset: "UTF-8",
+      # OGPの設定
+      og: {
+        title: "School Review",
+        type: "website",
+        url: request.original_url,
+        image: image_url("/images/slide8.png"),
+        site_name: "School Review",
+        description: "フィリピン／オーストラリアの語学学校レビューアプリ",
+        locale: "ja_JP"
+      } 
+    }
+  end
+
   def resource_name
     :user
   end
